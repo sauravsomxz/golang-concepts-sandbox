ef08e59f72b68efe65e03ee9eff2661f7960b9a6
Sourav Maharana <saurav.maharana07@gmail.com>
2024-06-01 18:10:21 +0530

    Developed more User Friendly Investment Calculator wiht messages

--- 1. Investment Calculator/investmenet_calculator.go
+++ 1. Investment Calculator/investmenet_calculator.go
@@ -6,9 +6,25 @@ import (
 )
 
 func main() {
-	investmentAmount, expectedReturnRate, years := 1000.0, 5.5, 10.0
+	const inflationRate float64 = 6.5
+
+	var investmentAmount float64
+	var expectedReturnRate float64
+	var years float64
+
+	fmt.Print("Enter investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Enter Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Enter Time Period (in years): ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
+	fmt.Println(futureValue - futureRealValue)
 }
