5da13331ff095f136c4296e23f69f7b1cd74abaf
Sourav Maharana <saurav.maharana07@gmail.com>
2024-06-17 13:21:49 +0530

    ATM Exercise

---
+++ 4. Atm/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var accountBalance = 1000
+
+	fmt.Println("Welcome to Bank")
+	fmt.Println("1. Check Account Balance")
+	fmt.Println("2. Deposit Money")
+	fmt.Println("3. Withdraw Money")
+	fmt.Println("4 Exit")
+
+	var choice int64
+	fmt.Print("Choose Option: ")
+	fmt.Scan(&choice)
+
+	if choice == 1 {
+		fmt.Println("Account Balance: ", accountBalance)
+	} else if choice == 2 {
+		fmt.Print("Your Deposit: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+
+		accountBalance = accountBalance + int(depositAmount)
+
+		fmt.Println("Updated Account Balance: ", accountBalance)
+	} else if choice == 3 {
+		fmt.Print("Your Withdrawl: ")
+		var withdrawAmount float64
+		fmt.Scan(&withdrawAmount)
+
+		accountBalance = accountBalance - int(withdrawAmount)
+
+		fmt.Println("Updated Account Balance: ", accountBalance)
+	} else {
+		fmt.Println("Thank you for using our bank.")
+	}
+}
